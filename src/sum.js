260a1f7fc79e6c6a294d5438905ce0816fb34f07
sweta <sweta.poudel26@gmail.com>
2021-07-29 16:04:58 +0545

    feat: add sum function

---
+++ src/sum.js
@@ -0,0 +1,16 @@
+function sum(array) {
+    let sums = 0;
+    if (array.length > 0) {
+      for (var i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+          sums = sums + sum(array[i]);
+        } else {
+          sums = sums + array[i];
+        }
+      }
+    }
+    return sums;
+  }
+  
+  module.exports = sum;
+  
